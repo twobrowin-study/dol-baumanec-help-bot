46547a17acde5925d8facad4bd7388e614982b13
Egor Dubrovin <dubrovin.en@ya.ru>
2023-05-14 23:42:18 +0300

    Добавлена функциональность поиска сотрудников ДОЛ Бауманец

--- python/ext/keyboard.py
+++ python/ext/keyboard.py
@@ -2,14 +2,32 @@ from spreadsheetbot.sheets.keyboard import KeyboardAdapterClass
 
 from spreadsheetbot.sheets.i18n import I18n
 
+from telegram import InlineKeyboardMarkup, InlineKeyboardButton
+
+import numpy as np
+
+KeyboardAdapterClass.CALLBACK_SCOUTS_TEMPLATE = 'keyboard_scouts_callback_{squad}'
+KeyboardAdapterClass.CALLBACK_SCOUTS_PATTERN  = 'keyboard_scouts_callback_*'
+KeyboardAdapterClass.CALLBACK_SCOUTS_PREFIX   = 'keyboard_scouts_callback_'
+
 KeyboardAdapterClass.default_pre_async_init = KeyboardAdapterClass._pre_async_init
 async def _pre_async_init(self):
     await self.default_pre_async_init()
-    self.GET_CHECKLIST = I18n.get_checklist
+    self.SCOUTS_FUNCTION = I18n.scouts
 KeyboardAdapterClass._pre_async_init = _pre_async_init
 
 KeyboardAdapterClass.default_process_df_update = KeyboardAdapterClass._process_df_update
 async def _process_df_update(self):
     await self.default_process_df_update()
-    self.get_checklist_keyboard_row = self._get(self.as_df.function == self.GET_CHECKLIST)
+    self.scouts_row = self.as_df.loc[
+        self.as_df.function == self.SCOUTS_FUNCTION
+    ].iloc[0]
+    self.squads = self.scouts_row.squads.split(';')
+    self.sqads_keyboard = InlineKeyboardMarkup([
+        [ 
+            InlineKeyboardButton(squad, callback_data=self.CALLBACK_SCOUTS_TEMPLATE.format(squad=squad))
+            for squad in (self.squads[idx:idx+4] if idx+4<len(self.squads) else self.squads[idx:])
+        ]
+        for idx in range(0,len(self.squads),4)
+    ])
 KeyboardAdapterClass._process_df_update = _process_df_update
